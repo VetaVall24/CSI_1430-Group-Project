1d40895b1357eb52578869e3df5b2f74e98d8f2e
VetaVall24 <109018265+VetaVall24@users.noreply.github.com>
2023-11-28 18:17:38 -0600

    Update Force.hpp

--- Force.hpp
+++ Force.hpp
@@ -1,1 +1,23 @@
-
+class force{
+	private:
+		double magnitude;
+		double direction;  // radian
+	
+	public:
+		force();
+		force(double m, double d);
+	
+		void setMagnitude(double m);
+		void setDirection(double d);
+		double getMagnitude()const;
+		double getDirection()const;
+	
+		force operator+(const force& other) const;
+		force operator=(const force& other);
+		force add(const force& other) const;
+		void apply(const force& other);
+	
+		void display(ostream& os)const{
+			os << getMagnitude() << " " << getDirection() << endl;
+		}
+};
