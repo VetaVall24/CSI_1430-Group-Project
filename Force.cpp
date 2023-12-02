c4c72fead6af41517034215c3b783b8ddf4fa037
jonahb023 <134671079+jonahb023@users.noreply.github.com>
2023-12-02 13:04:06 -0600

    Update Force.cpp

--- Force.cpp
+++ Force.cpp
@@ -1,66 +1,74 @@
+//
+//  force.cpp
+//  GroupProjectStarter
+//
+//  Created by Jonah Beck on 11/7/23.
+//
+
 #include "force.h"
 
 force::force(){
-	magnitude = 0;
-	direction = 0;
+    magnitude = 0;
+    direction = 0;
 }
 
 force::force(double m, double d){
-	magnitude = m;
-	direction = d;
+    magnitude = m;
+    direction = d;
 }
 
 force force::operator+(const force& other) const{
-	return add(other);
+    return add(other);
 }
 
 void force::apply(const force& other){
-	*this = add(other);
+    *this = add(other);
 }
 
 
 force force::add(const force& other) const{
-	force v;
-	double ax, ay;
-	double bx, by;
+    force v;
+    double ax, ay;
+    double bx, by;
     double theta, mag;
 
-	ax = magnitude * cos(direction);
-	bx = other.magnitude * cos(other.direction);
+    ax = magnitude * cos(direction);
+    bx = other.magnitude * cos(other.direction);
 
-	ay = magnitude * sin(direction);
-	by = other.magnitude * sin(other.direction);
+    ay = magnitude * sin(direction);
+    by = other.magnitude * sin(other.direction);
 
-	theta = atan((ay+by)/(ax+bx));
-	if((ay+by) < 0 && theta > 0) theta += PI;
-	if((ay+by) > 0 && theta < 0) theta += PI;
+    theta = atan((ay+by)/(ax+bx));
+    if((ay+by) < 0 && theta > 0) theta += PI;
+    if((ay+by) > 0 && theta < 0) theta += PI;
 
     mag   = sqrt(pow(ax + bx, 2) + pow(ay + by, 2));
 
-	v.magnitude = mag;
-	v.setDirection(theta);
-	return v;
+    v.magnitude = mag;
+    v.setDirection(theta);
+    return v;
 }
 
 void force::setMagnitude(double m){
-	magnitude = m;
+    magnitude = m;
 }
 void force::setDirection(double d){
     while(d > 2*PI ) d -= 2*PI;
     while(d < 0) d += 2*PI;
-	direction = d;
+    direction = d;
 }
 
 double force::getMagnitude()const{
-	return magnitude;
+    return magnitude;
 }
 
 double force::getDirection()const{
-	return direction;
+    return direction;
 }
 
 force force::operator=(const force& other){
-	setMagnitude(other.magnitude);
-	setDirection(other.direction);
+    setMagnitude(other.magnitude);
+    setDirection(other.direction);
     return *this;
 }
+
