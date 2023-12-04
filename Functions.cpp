15233e0d1c4da08531f059c85bfe50b7ce6d9dbe
VetaVall24 <109018265+VetaVall24@users.noreply.github.com>
2023-12-04 13:48:42 -0600

    Update Functions.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -16,35 +16,20 @@ void NewRow(const int& min, string shapes[], int healths[]) {
 }
 
 void Collision();
+
 string CreateObject(int x, int loc) {
-    int width, height, locX, locY = 3;
-    color c;
     string name;
     
     if(x == 1 || x == 2) {
-        width = height = 1;
         name = "circle";
     }
     else if (x == 3) {
-        width = height = 1;
         name = "square";
     }
     else {
-        width = height = 1;
         name = "triangle";
     }
     
-    if(loc = 0) {
-        locX = 1;
-    }
-    else if(loc = 1) {
-        locX = 2;
-    }
-    else {
-        locX = 3;
-    }
-    
-    Shape(locX, locY, width, height, c);
     return name;
 }
 
